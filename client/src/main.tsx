4753228787f874ba9058edbac01ddb1e81cc9db4
ReflectivEI <ReflectivEI.ai@gmail.com>
2025-12-04 21:18:59 -0800

    Wrap App component with ThemeProvider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "./components/theme-provider";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Root element #root not found");
+}
+
+createRoot(container).render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
